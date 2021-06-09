983f068fbbf2146a1b3ad47efbf1c24d62e79fcb
Jasonsetio <jasonsetio30@gmail.com>
2021-06-09 21:36:52 +0700

    2

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IData {
+  id: string;
+  name: string;
+  email: string;
+}
+
+interface IAppState{
+  showTable: boolean;
+}
+
+interface IAppProps{
+  data:IData[]
+}
+
+class App extends React.Component<IAppProps, IAppState> {
+public toogleShowTable = () => {
+  const { showTable } = this.state;
+  this.setState({showTable: !showTable});
+}
 }
 
 export default App;
+
+<button onClick= { this.toogleShowTable}></button>
